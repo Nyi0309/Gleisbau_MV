d8aa29ed2f0078ac28722bfbaca905c3b08ae6a2
Paul von Labu <koch0309paul@gmail.com>
2025-03-21 10:55:18 +0100

    header anpassungen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ const links = [
 
 const Header = () => {
     return (
-        <header className='bg-white py-4 text-secondary-dark '>
+        <header className='bg-white py-4 text-secondary-dark border-b border-primary/20 shadow-sm'>
             <div className="container mx-auto">
 
                 <div className='flex items-center justify-between'>
@@ -50,9 +50,9 @@ const Header = () => {
                             })}
                         </ul>
                         {/**button */}
-                        <button className='w-[200px] h-[54px] py-[5px] pl-[10px] rounded-sm pr-[5px] flex items-center justify-between min-w-[200px] group bg-white cursor-pointer'>
+                        <button className='w-[200px] h-[54px] py-[5px] pl-[10px] rounded-sm pr-[5px] flex items-center border border-primary justify-between min-w-[200px] group bg-white cursor-pointer'>
                             <div className='flex-1 text-center tracking-[1.2px] font-primary font-bold text-primary text-sm uppercase'>Kontakt</div>
-                            <div className='w-11 h-11 bg-primary flex items-center justify-center'>
+                            <div className='w-11 h-11 bg-primary  flex items-center justify-center'>
                                 <RiArrowRightUpLine className='text-xl text-white group-hover:rotate-45 transition-all duration-200' />
                             </div>
                         </button>
